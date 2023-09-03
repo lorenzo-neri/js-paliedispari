c6c03cf0468d87db5699f47ba0646ec02dff75fd
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-03 13:03:10 +0200

    add el in page

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,42 +15,47 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 //PARI E DISPARI
 
 const buttonEvenOrOddEl = document.getElementById('btn_even_odd');
-const formEvenOrOdd = document.getElementById('form_even_odd');
+//const formEvenOrOdd = document.getElementById('form_even_odd');
+const buttonClear = document.getElementById('clear')
 
-formEvenOrOdd.addEventListener('submit', function (e) {
+buttonEvenOrOddEl.addEventListener('click', function (e) {
 
     //impedisco refresh pagina
     e.preventDefault();
+
+    //per non poter spammare risultati in  pagina
+    buttonEvenOrOddEl.style.display = 'none';
+    buttonClear.style.display = 'inline-block';
 
     // ### far scegliere pari o dispari ###
     //const userChoice = (prompt('Pari o dispari?')).toLowerCase();
 
     const userChoice = document.getElementById('user_word').value.toLowerCase();
 
-   /*  if (userChoice != 'pari' && userChoice != 'dispari') {
+    if (userChoice != 'pari' && userChoice != 'dispari') {
         alert('Si prega di scegliere tra pari o dispari');
-        location.reload();
-    } */
+        location.reload(); //ricarico la pagina
+    }
 
     console.log(userChoice);
 
     // ### far inserire un numero da 1 a 5 ###
     //const userNumber = Number(prompt('Scegli un numero da 1 a 5'));
 
-    const userNumber = document.getElementById('user_number').value;
-    
-    /*if (userNumber != 1
+    const userNumber = Number(document.getElementById('user_number').value);
+
+    if (userNumber != 1
         && userNumber != 2
         && userNumber != 3
         && userNumber != 4
         && userNumber != 5) {
         alert('Si prega di scegliere un numero da 1 a 5');
-        location.reload(userChoice);
-    }*/
-    
+        location.reload(userChoice); //ricarico la pagina
+    }
+
     console.log(userNumber);
 
-    
+
     //generare un numero da 1 a 5 per il computer, con una funzione
     function numberGeneratorFromOneToFive() {
         return Math.floor((Math.random() * 5) + 1);
@@ -59,31 +64,46 @@ formEvenOrOdd.addEventListener('submit', function (e) {
     //sommare i numeri 
     const sum = userNumber + (numberGeneratorFromOneToFive());
     let cpuNumber = sum - userNumber       //facoltativo
-    console.log(cpuNumber);       //facoltativo
-    let cpuNumberEl = cpuNumber(document.getElementById('cpu_number'));
-    
-    // ####################### unfinished ######################## //
-    // ####################### unfinished ######################## //
-    // ####################### unfinished ######################## //
+    console.log(cpuNumber);      //facoltativo
+    let cpuNumberEl = document.querySelector('.cpu_number');
+    cpuNumberEl.append(`Il numero del computer è: ${cpuNumber}`);
 
     console.log(sum);
+    let sumEl = document.querySelector('.sum');
+    sumEl.append(`La somma è: ${sum}`)
 
     //stabilire se la somma dei due numeri è pari o dispari, con una funzione
     function isEven(number) {
         return number % 2 === 0;
     }
 
+    const result = document.getElementById('result');
+
     //dichiarare chi ha vinto
     if (isEven(sum) && userChoice === 'pari') {
         console.log('La somma è pari!! Hai vinto!!😃');
+        result.append('La somma è pari!! Hai vinto!!😃');
+
     } else if (isEven(sum) && userChoice !== 'pari') {
         console.log('La somma è pari!! Hai perso!!😔');
+        result.append('La somma è pari!! Hai perso!!😔');
+
     } else if (!isEven(sum) && userChoice === 'dispari') {
         console.log('La somma è dispari! Hai vinto!!😃');
+        result.append('La somma è dispari! Hai vinto!!😃');
+
     } else if (!isEven(sum) && userChoice !== 'dispari') {
         console.log('La somma è dispari! Hai perso!!😔');
-    } */
+        result.append('La somma è dispari! Hai perso!!😔');
+    }
 })
+
+buttonClear.addEventListener('click', function (e) {
+    location.reload()
+})
+
+
+
 
 
 //PALINDROMA
